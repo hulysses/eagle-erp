2089e45fc0fb5d31e87d49d3d89ed57ec2683fdf
hulysses <hulyssesmaga@gmail.com>
2026-06-14 21:32:04 -0300

    Initial commit

---
+++ src/main/java/com/hulysses/eagleerp/Eagle.java
@@ -0,0 +1,13 @@
+package com.hulysses.eagleerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eagle {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eagle.class, args);
+    }
+
+}
---
+++ src/test/java/com/hulysses/eagleerp/EagleerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulysses.eagleerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
