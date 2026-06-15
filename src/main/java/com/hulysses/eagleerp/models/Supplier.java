5b8c4ef0e751762711785283d51f4bcb9cc07c52
hulysses <hulyssesmaga@gmail.com>
2026-06-14 22:19:21 -0300

    Remove Person class and refactor Supplier class

--- src/main/java/com/hulysses/eagleerp/models/Supplier.java
+++ src/main/java/com/hulysses/eagleerp/models/Supplier.java
@@ -1,11 +1,48 @@
 package com.hulysses.eagleerp.models;
 
-public class Supplier extends Person {
+public class Supplier {
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
     private String cnpj;
 
     public Supplier(String name, String phone, String cnpj) {
-        super(name, phone);
+        this.name = name;
+        this.phone = phone;
         this.cnpj = cnpj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getCnpj() {
