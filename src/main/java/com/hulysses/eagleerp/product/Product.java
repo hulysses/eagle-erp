776a92eef3dc72f6fd1d687a8b8cbc1d16438e27
hulysses <hulyssesmaga@gmail.com>
2026-07-04 16:05:25 -0300

    refactor Supplier and Product class

--- src/main/java/com/hulysses/eagleerp/product/Product.java
+++ src/main/java/com/hulysses/eagleerp/product/Product.java
@@ -24,23 +24,11 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Supplier getSupplier() {
         return supplier;
     }
 
-    public void setSupplier(Supplier supplier) {
-        this.supplier = supplier;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
     }
 }
--- src/main/java/com/hulysses/eagleerp/supplier/Supplier.java
+++ src/main/java/com/hulysses/eagleerp/supplier/Supplier.java
@@ -8,9 +8,11 @@ public class Supplier {
     private String address;
     private String cnpj;
 
-    public Supplier(String name, String phone, String cnpj) {
+    public Supplier(String name, String email, String phone, String address, String cnpj) {
         this.name = name;
+        this.email = email;
         this.phone = phone;
+        this.address = address;
         this.cnpj = cnpj;
     }
 
@@ -22,39 +24,19 @@ public class Supplier {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getCnpj() {
         return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
     }
 }
